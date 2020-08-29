9ee3a7bd5e235f0fd5a218f5ae124c56e5d50d13
luochao <luochao@yy.com>
2020-08-29 20:52:33 +0800

    fix: 优化代码

--- packages/app-component/src/app-component.tsx
+++ packages/app-component/src/app-component.tsx
@@ -1,14 +1,13 @@
 import React, { FC } from 'react'
 
 interface AppProps {
-    msg: string
+  msg: string
 }
 
 const App: FC<AppProps> = ({ msg }) => {
-
-    return (
-        <h1>hello {msg ? msg : 'react' }!</h1>
-    )
+  return (
+    <h1>hello {msg ? msg : 'react' }!</h1>
+  )
 }
 
 export default App
--- packages/localstorage/src/index.ts
+++ packages/localstorage/src/index.ts
@@ -1,38 +1,36 @@
-
-function parse<T> (data: any): T | null {
-    try {
-        return JSON.parse(data)
-    } catch (err) {
-        return null
-    }
+function parse < T > (data: any): T | null {
+  try {
+    return JSON.parse(data)
+  } catch (err) {
+    return null
+  }
 }
 
-function stringify (data: any): string {
-    return JSON.stringify(data)
+function stringify(data: any): string {
+  return JSON.stringify(data)
 }
 
-function getValue<T> (key: string): T | null {
-    const data = window.localStorage.getItem(key)
-    return parse<T>(data)
+function getValue < T > (key: string): T | null {
+  const data = window.localStorage.getItem(key)
+  return parse < T > (data)
 }
 
-function setValue (key: string, data: any) {
-    window.localStorage.setItem(key, stringify(data))
+function setValue(key: string, data: any) {
+  window.localStorage.setItem(key, stringify(data))
 }
 
-function removeValue (key: string) {
-    window.localStorage.removeItem(key)
+function removeValue(key: string) {
+  window.localStorage.removeItem(key)
 }
 
 interface LocalStorage {
-    getValue<T> (key: string): T
-    setValue(key: string, data: any): void
-    removeValue(key: string): void
+  getValue < T > (key: string): T
+  setValue(key: string, data: any): void
+  removeValue(key: string): void
 }
 
 export default {
-    getValue,
-    setValue,
-    removeValue
+  getValue,
+  setValue,
+  removeValue
 } as LocalStorage
-
